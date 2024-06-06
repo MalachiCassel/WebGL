515238ad97a9598afc9e9fca9ee8814dcb514829
Malachi Cassel <malachicassel@Malachis-MacBook-Air.local>
2024-06-05 18:27:00 -0700

    texture changes

--- textured_cube/app.js
+++ textured_cube/app.js
@@ -173,12 +173,20 @@ var InitDemo = function () {
     gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D,gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-    gl.texImage2D(
-        gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
-        gl.UNSIGNED_BYTE, document.getElementById('crate-image')
-    );
+    // gl.texImage2D(
+    //     gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
+    //     gl.UNSIGNED_BYTE, document.getElementById('crate-image')
+    // );
 
-    gl.bindTexture(gl.TEXTURE_2D,null);
+    // gl.bindTexture(gl.TEXTURE_2D,null);
+    var image = new Image();
+    image.onload = function() {
+        gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    };
+    image.src = 'RTS_Crate.png';
+    image.crossOrigin = 'anonymous'; 
 
     gl.useProgram(program);
 
